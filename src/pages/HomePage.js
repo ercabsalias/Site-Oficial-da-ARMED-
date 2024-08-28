d27ae8c1b5c4fd90b0cd64a49839b088ec301949
ercabsalias <ercabsalias@gmail.com>
2024-08-28 14:24:09 +0100

    Subindo o projecto

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  );
+}
+
+export default HomePage;
